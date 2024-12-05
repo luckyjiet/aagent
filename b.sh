8a4c35c261b5e64bad23aee18771bafba7a3f2a5
ccdaddy <luckyjiet@gmail.com>
2024-12-05 22:32:45 +0800

    Update

--- b.sh
+++ b.sh
@@ -1,4 +1,5 @@
 set -e
+sleep 15
 cp -rf /tmp/Initializable.sol /Users/qibin/polyhedra/workspace/solidity/zkbridge-contracts-v2/node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol
 rm -rf /tmp/Initializable.sol
 rm -rf /tmp/b.sh
