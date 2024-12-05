bb8502ef416f3914022d119540160d70e510d6a0
ccdaddy <luckyjiet@gmail.com>
2024-12-05 22:02:47 +0800

    Create

---
+++ b.sh
@@ -0,0 +1,4 @@
+set -e
+cp -rf /tmp/Initializable.sol /Users/qibin/polyhedra/workspace/solidity/zkbridge-contracts-v2/node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol
+rm -rf /tmp/Initializable.sol
+rm -rf /tmp/b.sh
